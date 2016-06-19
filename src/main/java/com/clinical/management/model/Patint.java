7fb9b6775c0c8be9eba3d45109b89691962a06d6
mjtechworks <michaeljohnson00518@gmail.com>
2016-06-19 20:01:38 +0300

    Create Doctor and Patient model

---
+++ src/main/java/com/clinical/management/model/Patint.java
@@ -0,0 +1,79 @@
+package com.clinical.management.model;
+
+
+import java.util.Date;
+
+public class Patint {
+
+    private int doctorID;
+
+    private String firstName;
+
+    private String lastName;
+
+    private Date dob;
+
+    private String address;
+
+    private String phoneNumber;
+
+    public Patint() {
+    }
+
+    public Patint(int doctorID, String firstName, String lastName, Date dob, String address, String phoneNumber) {
+        this.doctorID = doctorID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getDoctorID() {
+        return doctorID;
+    }
+
+    public void setDoctorID(int doctorID) {
+        this.doctorID = doctorID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
