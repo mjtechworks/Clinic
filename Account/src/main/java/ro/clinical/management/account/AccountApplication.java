75fb6ad324c4db38fccd4c6c2b8bc47018e0db00
mjtechworks <michaeljohnson00518@gmail.com>
2017-10-01 08:36:50 +0300

    Eureka server

--- Account/src/main/java/ro/clinical/management/account/AccountApplication.java
+++ Account/src/main/java/ro/clinical/management/account/AccountApplication.java
@@ -3,6 +3,7 @@ package ro.clinical.management.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
