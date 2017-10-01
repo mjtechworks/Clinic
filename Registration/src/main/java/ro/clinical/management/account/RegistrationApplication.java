88bcf575b58467295c6cfc6d715c1f96cbabd601
mjtechworks <michaeljohnson00518@gmail.com>
2017-10-01 08:59:18 +0300

    Registration Server

--- Registration/src/main/java/ro/clinical/management/account/RegistrationApplication.java
+++ Registration/src/main/java/ro/clinical/management/account/RegistrationApplication.java
@@ -7,10 +7,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class AccountApplication {
+public class RegistrationApplication {
     public static void main(String[] args) {
         // Tell Boot to look for registration-server.yml
         System.setProperty("spring.config.name", "registration-server");
-        SpringApplication.run(AccountApplication.class, args);
+        SpringApplication.run(RegistrationApplication.class, args);
     }
 }
