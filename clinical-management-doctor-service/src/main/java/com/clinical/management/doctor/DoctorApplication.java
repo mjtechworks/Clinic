e4f6364950ff6c0a7c39dd64d3f63d9335b2ac58
mjtechworks <michaeljohnson00518@gmail.com>
2017-10-16 23:46:42 +0300

    Start doctor service

---
+++ clinical-management-doctor-service/src/main/java/com/clinical/management/doctor/DoctorApplication.java
@@ -0,0 +1,12 @@
+package com.clinical.management.doctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorApplication.class, args);
+    }
+}
