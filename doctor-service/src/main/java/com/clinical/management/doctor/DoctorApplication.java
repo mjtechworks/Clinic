77368bab9cf648b28de448c3f5a686dc08108909
mjtechworks <michaeljohnson00518@gmail.com>
2017-10-29 14:50:28 +0200

    Add service discovery to each services

--- doctor-service/src/main/java/com/clinical/management/doctor/DoctorApplication.java
+++ doctor-service/src/main/java/com/clinical/management/doctor/DoctorApplication.java
@@ -2,8 +2,10 @@ package com.clinical.management.doctor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DoctorApplication {
 
     public static void main(String[] args) {
