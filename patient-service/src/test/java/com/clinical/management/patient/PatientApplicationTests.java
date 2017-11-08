1764f7ff380127461eac49401b5c749fcd5496cf
mjtechworks <michaeljohnson00518@gmail.com>
2017-11-08 22:24:39 +0200

    Patient service - context loads service

---
+++ patient-service/src/test/java/com/clinical/management/patient/PatientApplicationTests.java
@@ -0,0 +1,19 @@
+package com.clinical.management.patient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = PatientApplication.class)
+@WebAppConfiguration
+public class PatientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
