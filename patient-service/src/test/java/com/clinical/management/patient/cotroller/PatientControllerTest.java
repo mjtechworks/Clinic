1c0cd246258303739e8abc46847fe6a0f4c62c33
mjtechworks <michaeljohnson00518@gmail.com>
2017-11-08 23:43:45 +0200

    Patient service - add controller test

---
+++ patient-service/src/test/java/com/clinical/management/patient/cotroller/PatientControllerTest.java
@@ -0,0 +1,80 @@
+package com.clinical.management.patient.cotroller;
+
+import com.clinical.management.patient.PatientApplication;
+import com.clinical.management.patient.controller.PatientController;
+import com.clinical.management.patient.domain.Patient;
+import com.clinical.management.patient.repository.PatientRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import javax.ws.rs.core.MediaType;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = PatientApplication.class)
+@WebAppConfiguration
+public class PatientControllerTest {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    @InjectMocks
+    private PatientController patientController;
+
+    @Mock
+    private PatientRepository patientRepository;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(patientController).build();
+    }
+
+    @Test
+    public void getAccountById() throws Exception {
+        Patient patient = new Patient();
+
+        patient.setId("55");
+        patient.setEmail("test@test.com");
+        patient.setFullName("Test Test");
+
+        when(patientRepository.findOne(patient.getId())).thenReturn(patient);
+
+        mockMvc.perform(get("/" + patient.getId()))
+                .andExpect(jsonPath("$.fullName").value(patient.getFullName()))
+                .andExpect(jsonPath("$.email").value(patient.getEmail()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void saveAccount() throws Exception {
+        Patient patient = new Patient();
+
+        patient.setId("55");
+        patient.setEmail("test@test.com");
+        patient.setFullName("Test Test");
+
+        String json = mapper.writeValueAsString(patient);
+
+        mockMvc.perform(post("").contentType(MediaType.APPLICATION_JSON).content(json))
+                .andExpect(status().isOk());
+
+    }
+
+}
