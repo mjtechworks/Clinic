3d14b6bf5f587a3757448fb631e643ff60d4079b
mjtechworks <michaeljohnson00518@gmail.com>
2017-11-26 23:29:31 +0200

    user-interface -> add angular material and change interface

--- user-interface/src/app/app.module.ts
+++ user-interface/src/app/app.module.ts
@@ -6,12 +6,18 @@ import {AppComponent} from './app.component';
 import {PatientComponent} from './patient/patient.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import { AppointmentComponent } from './appointment/appointment.component';
+import {AppointmentComponent} from './appointment/appointment.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatButtonModule, MatInputModule, MatTableModule} from "@angular/material";
 
 
 @NgModule({
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatTableModule,
+        MatInputModule,
         FormsModule,
         HttpClientModule
     ],
