9aa107dc884efe74ffbbcb550b1d3af29e01bb65
mjtechworks <michaeljohnson00518@gmail.com>
2018-01-07 13:51:27 +0200

    user-interface -> side nav barr

--- user-interface/src/app/app.component.ts
+++ user-interface/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {AuthenticationService} from "./service/authentication.service";
 import {Router} from "@angular/router";
 
@@ -10,6 +10,9 @@ import {Router} from "@angular/router";
 })
 export class AppComponent {
 
+    @ViewChild('sidenav') sidenav: ElementRef;
+    @ViewChild('main') main: ElementRef;
+
     constructor(private service: AuthenticationService, private router: Router) {
     }
 
@@ -19,6 +22,16 @@ export class AppComponent {
 
     private navigate(url: String) {
         this.router.navigate([url]);
+    }
+
+    private openNav() {
+        this.sidenav.nativeElement.style.width = "250px";
+        this.main.nativeElement.style.marginLeft = "250px";
+    }
+
+    private closeNav() {
+        this.sidenav.nativeElement.style.width = "0";
+        this.main.nativeElement.style.marginLeft = "0";
     }
 
 }
