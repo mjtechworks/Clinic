371d1a04b856669f92f34d10f83267457da9ed73
mjtechworks <michaeljohnson00518@gmail.com>
2018-01-20 00:35:25 +0200

    user-interface -> doctor update

--- user-interface/src/app/doctor-update/doctor-update.component.ts
+++ user-interface/src/app/doctor-update/doctor-update.component.ts
@@ -1,16 +1,42 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {AuthenticationService} from "../service/authentication.service";
+import {Doctor} from "../domain/doctor";
+import {DoctorService} from "../service/doctor.service";
+import {MatSnackBar} from "@angular/material";
+import {PatientService} from "../service/patient.service";
 
 @Component({
-  selector: 'app-doctor-update',
-  templateUrl: './doctor-update.component.html',
-  styleUrls: ['./doctor-update.component.css'],
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-doctor-update',
+    templateUrl: './doctor-update.component.html',
+    styleUrls: ['./doctor-update.component.css'],
+    providers: [DoctorService, AuthenticationService],
+    encapsulation: ViewEncapsulation.None
 })
 export class DoctorUpdateComponent implements OnInit {
 
-  constructor() { }
+    private model: Doctor;
 
-  ngOnInit() {
-  }
+    constructor(private authService: AuthenticationService, private doctorService: DoctorService, public snackBar: MatSnackBar) {
+    }
+
+    ngOnInit() {
+        this.authService.checkCredentials();
+        this.model = new Doctor();
+        this.authService.getCurrentAccount().subscribe(data => {
+            this.model = data;
+        });
+    }
+
+    private onSubmit() {
+        this.doctorService.updateDoctor(this.model).subscribe(data => {
+            this.displayMessage("The account was updated successfully !");
+        });
+    }
+
+    private displayMessage(message) {
+        this.snackBar.open(message, "", {
+            duration: 5000,
+        });
+    }
 
 }
--- user-interface/src/app/service/doctor.service.ts
+++ user-interface/src/app/service/doctor.service.ts
@@ -2,17 +2,25 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Doctor} from "../domain/doctor";
 import {Observable} from "rxjs/Observable";
+import {Service} from "./service";
+import {AuthenticationService} from "./authentication.service";
 
 @Injectable()
-export class DoctorService {
+export class DoctorService extends Service {
 
     private createDoctorUrl = "api/doctors/create";
+    private updateDoctorUrl = "api/doctors/update";
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, private authService: AuthenticationService) {
+        super(authService);
     }
 
     public createDoctor(doctor: Doctor): Observable<Object> {
         return this.http.post<Object>(this.createDoctorUrl, doctor);
+    }
+
+    public updateDoctor(doctor: Doctor): Observable<Doctor> {
+        return this.http.put<Doctor>(this.updateDoctorUrl, doctor, super.getHeader());
     }
 
 }
