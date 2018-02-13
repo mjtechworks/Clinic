8277edb1dd68e600ab5639effd3a56b560e30f16
mjtechworks <michaeljohnson00518@gmail.com>
2018-02-13 22:55:42 +0200

    appointment-service -> change doctor request

--- appointment-service/src/main/java/com/clinical/management/appointment/client/DoctorClientService.java
+++ appointment-service/src/main/java/com/clinical/management/appointment/client/DoctorClientService.java
@@ -3,14 +3,14 @@ package com.clinical.management.appointment.client;
 import com.clinical.management.appointment.domain.Doctor;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "doctor-service")
 public interface DoctorClientService {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/doctors/{email}", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    Doctor getDoctor(@PathVariable("email") String email);
+    @RequestMapping(method = RequestMethod.GET, value = "/doctors/find", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    Doctor getDoctor(@RequestParam("email") String email);
 
 }
